591e66d9c2808d63fb02da63dfb9de706af1983a
samuelcostta <99897548+samuelcostta@users.noreply.github.com>
2023-06-15 21:17:01 -0300

    Configurando a rota da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'pacientes'},
+  //{ path: '', pathMatch: 'full', redirectTo: 'pacientes'},
+
+  { path: '', pathMatch: 'full', redirectTo: 'login'},
+
   {
     path: 'pacientes',
     loadChildren: () => import('./pacientes/pacientes.module').then(m => m.PacientesModule)
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
   }
 ];
 
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './../shared/shared.module';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    SharedModule
+  ]
+})
+export class LoginModule { }
