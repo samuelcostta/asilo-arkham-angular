607ce56e167bd8a2439cc586a1344674c9a98635
samuelcostta <samuelcostta2306@gmail.com>
2023-06-20 18:56:32 -0300

    Implementação da tela de login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+
+  export class LoginComponent implements OnInit {
+    username: string="";
+    password: string="";
+    error: string="";
+    mostrarMenuEmitter = new EventEmitter<boolean>();
 
   form = this.formBuilder.group({
     id: [0],
@@ -20,11 +27,22 @@ export class LoginComponent implements OnInit{
     ) {}
 
   ngOnInit(): void {
+
+
   }
 
   onEntrar() {
-    //console.log('oi')
-    this.router.navigate(['/pacientes']);
-  }
+ // verificar se o usuário e a senha estão corretos
+ if (this.username === "Maria" && this.password === "123", this.username === "Alencar" && this.password === "321") {
+  this.mostrarMenuEmitter.emit(true);
+  this.router.navigate(['/pacientes']);
+} else {
+  this.error = "Usuário ou senha incorretos.";
+  this.mostrarMenuEmitter.emit(false);
+}
 }
 
+
+    //console.log('oi')
+
+  }
